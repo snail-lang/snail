3e3dda4ed8923b7dbc916a668004230915ee6783
nilq <i.am@nilq.dk>
2017-09-15 09:30:08 +0200

    Add stream traveler

--- src/snail/syntax/parser/mod.rs
+++ src/snail/syntax/parser/mod.rs
@@ -1,8 +1,10 @@
 pub mod error;
 pub mod ast;
+pub mod traveler;
 
 pub use super::lexer;
 pub use self::error::*;
 pub use self::ast::*;
+pub use self::traveler::*;
 
 pub type ParserResult<T> = Result<T, ParserError>;
